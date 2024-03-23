b8ddc6e4b9b4e6786f09347a396505d3a9601501
MLoomer <112742863+MLoomer@users.noreply.github.com>
2024-03-22 19:49:32 -0500

    OOP

--- delivery_service.py
+++ delivery_service.py
@@ -1,88 +1,100 @@
 
-def deliverPackages(trucks, travel_map):
-    for truck in trucks:
-        empty = deliver_a_package(truck, travel_map)
-        if empty is True:
-            print(f"Truck completed trip, total distance: {truck.distance}")
-            trucks.remove(truck)
-    # while trucks are not empty
-    if trucks:
-        deliverPackages(trucks, travel_map)
-    else:
-        return None
+class TruckService:
+    def __init__(self, truck, map):
+        self.truck = truck
+        self.map = map
+        self.rowOffset, self.colOffset, self.valuemap = self.remove_labels()
 
-def get_remaining_id_addresses(truck):
-    remaining_addresses = []
-    for id in truck.package_IDs:
-        addr = truck.packages.lookup(id)[0]
-        remaining_addresses.append([id, addr])
-    return remaining_addresses
+    def get_remaining_id_addresses(self):
+        remaining_addresses = []
+        for id in self.truck.package_IDs:
+            addr = self.truck.packages.lookup(id)[0]
+            remaining_addresses.append([id, addr])
+        return remaining_addresses
 
-def get_remaining_addresses(remaining_addresses):
-    arr = []
-    for address in remaining_addresses:
-        arr.append(address[1])
-    return arr
+    def get_remaining_addresses(self, remaining_addresses):
+        arr = []
+        for address in remaining_addresses:
+            arr.append(address[1])
+        return arr
 
-def get_row_index(value, arr):
-    for i, row in enumerate(arr):
-        if value in row[0]:
-            return i-1
+    def remove_labels(self):
+        arr = []
+        rowOffset = 1
+        value_map = self.map.copy()
+        colOffset = 2
+        for i in range(len(value_map)):
+            if (i > 0):
+                arr.append(value_map[i][colOffset:])
+        return rowOffset, colOffset, arr
 
-def getXYValue(rowInd, i, valuemap):
-    #print(f"rowInd {rowInd}, i = {i}")
-    #print(f"travelmap: {valuemap[rowInd]}")
-    if valuemap[rowInd][i] == "":
-        return valuemap[i][rowInd]
-    return valuemap[rowInd][i]
+    def get_matching_ids(self, address, id_address):
+        arr = []
+        for row in id_address:
+            if address in row[1]:
+                arr.append(row[0])
+        return arr
 
-def remove_labels(travel_map):
-    arr = []
-    rowOffset = 1
-    value_map = travel_map.copy()
-    colOffset = 2
-    for i in range(len(value_map)):
-        if (i > 0):
-            arr.append(value_map[i][colOffset:])
-    return rowOffset, colOffset, arr
+    def deliver_a_package(self):
+        current_address = self.truck.address
+        id_address = self.get_remaining_id_addresses()
+        remaining_addresses = self.get_remaining_addresses(id_address)
+        address_to_go, distance = self.nearest_drive_alg(current_address, remaining_addresses)
+        self.truck.address = address_to_go
+        self.truck.distance += float(distance)
+        print(f"delivered to {self.truck.address}, distance is {self.truck.distance}")
+        self.truck.removePackages(self.get_matching_ids(self.truck.address, id_address))
+        if (len(self.truck.package_IDs) == 0): return True
 
-def get_address(index, travel_map):
-    return travel_map[0][index]
+    def get_address(self, index, travel_map):
+        return travel_map[0][index]
 
-def get_matching_ids(address, id_address):
-    arr = []
-    for row in id_address:
-        if address in row[1]:
-            arr.append(row[0])
-    return arr
+    def get_row_index(self, value, arr):
+        for i, row in enumerate(arr):
+            if value in row[0]:
+                return i - 1
+
+    def getXYValue(self, rowInd, i, valuemap):
+        # print(f"rowInd {rowInd}, i = {i}")
+        # print(f"travelmap: {valuemap[rowInd]}")
+        if valuemap[rowInd][i] == "":
+            return valuemap[i][rowInd]
+        return valuemap[rowInd][i]
+
+    def nearest_drive_alg(self, current_address, remaining_addresses):
+        rowInd = self.get_row_index(current_address, self.map)
+        min = 9999
+        index = 9999
+        for i in range(len(self.valuemap[rowInd])):
+            value = float(self.getXYValue(rowInd, i, self.valuemap))
+            if (value < min) and (rowInd != i):
+                addrr = self.get_address(i + self.colOffset, self.map).split("\n")[1].lstrip()
+                if addrr in remaining_addresses:
+                    min = value
+                    index = i
+        if (index == 9999): print("failed to find match")
+        new_address = self.get_address(index + self.colOffset, self.map).split("\n")[1].lstrip()
+        return new_address, min
+
+class DeliveryService:
+    def __init__(self, trucks, map):
+        self.trucks = trucks
+        self.map = map
 
 
-def strip(address):
-    return address.split("\n")[1].lstrip()
+    def deliverPackages(self):
 
-def nearest_drive_alg(current_address, remaining_addresses, travel_map):
-    rowInd = get_row_index(current_address, travel_map)
-    min = 9999
-    index = 9999
-    rowOffset, colOffset, valuemap = remove_labels(travel_map)
-    for i in range(len(valuemap[rowInd])):
-        value = float(getXYValue(rowInd, i, valuemap))
-        if (value < min) and (rowInd != i):
-            addrr = strip(get_address(i+colOffset, travel_map))
-            if addrr in remaining_addresses:
-                min = value
-                index = i
-    if (index == 9999): print("failed to find match")
-    new_address = strip(get_address(index+colOffset, travel_map))
-    return new_address, min
+        for truck in self.trucks:
+            delivery = TruckService(truck, self.map)
+            empty = delivery.deliver_a_package()
+            if empty is True:
+                print(f"Truck completed trip, total distance: {truck.distance}")
+                self.trucks.remove(truck)
+        # while trucks are not empty
+        if self.trucks:
+            self.deliverPackages()
+        else:
+            return None
 
-def deliver_a_package(truck, travel_map):
-    current_address = truck.address
-    id_address = get_remaining_id_addresses(truck)
-    remaining_addresses = get_remaining_addresses(id_address)
-    address_to_go, distance = nearest_drive_alg(current_address, remaining_addresses, travel_map)
-    truck.address = address_to_go
-    truck.distance += float(distance)
-    print(f"delivered to {truck.address}, distance is {truck.distance}")
-    truck.removePackages(get_matching_ids(truck.address, id_address))
-    if (len(truck.package_IDs) == 0): return True
+
+
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from load_packages import get_packages
 from load_packages import loadTrucks
-from delivery_service import deliverPackages
 from Distances import get_distances
+from delivery_service import TruckService
+from delivery_service import DeliveryService
 
 def main():
     packages = get_packages()
     trucks = loadTrucks(packages)
     travel_map = get_distances()
-    deliverPackages(trucks, travel_map)
+    delivery = DeliveryService(trucks, travel_map)
+    delivery.deliverPackages()
     print(trucks)
     #TODO: instead of 'running', build array of package order, then give to interface to use?
 
